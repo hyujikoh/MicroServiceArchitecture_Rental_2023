50a25aff0f3b4d36372ae2b58eeb879c6d6002eb
hyujikoh <dvum0045@gmail.com>
2023-02-12 17:38:48 +0900

    초기화

--- src/main/java/com/my/rental/domain/enumeration/RentalStatus.java
+++
@@ -1,8 +0,0 @@
-package com.my.rental.domain.enumeration;
-
-/**
- * The RentalStatus enumeration.
- */
-public enum RentalStatus {
-    RENT_AVAILABLE, RENT_UNAVAILABLE
-}
--- src/main/java/com/my/rental/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package com.my.rental.web.rest;
